b78e1a02b38d6e01782cc17755baf1782e79a88b
Jaiveer Singh <j.singh@berkeley.edu>
2021-04-21 00:06:23 -0700

    Local updates to checkpoints

--- baselines_test.py
+++ baselines_test.py
@@ -2,33 +2,47 @@ import os
 import time
 
 import pybullet_envs
-from stable_baselines3 import PPO
+from stable_baselines3 import PPO, A2C
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.vec_env import VecNormalize
+from stable_baselines3.common.callbacks import CheckpointCallback
 
 ENV_NAME = "HopperBulletEnv-v0"
-log_dir = "/tmp/"
-stats_path = os.path.join(log_dir, ENV_NAME + "vec_normalize.pkl")
+MODEL_PREFIX = "a2c"
+_model = A2C
 
-training = True
-loadPrevious = True
+stats_path = f"./{MODEL_PREFIX}_{ENV_NAME}_stats.pkl"
+
+training = False
+loadPrevious = False
 if training:
     env = make_vec_env(ENV_NAME, n_envs=1)
     env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.0)
 
     model = (
-        PPO.load(log_dir + "ppo_" + ENV_NAME)
+        _model.load(f"./{MODEL_PREFIX}_{ENV_NAME}_model", env)
         if loadPrevious
-        else PPO("MlpPolicy", env, verbose=1)
+        else _model(
+            "MlpPolicy",
+            env,
+            verbose=1,
+            tensorboard_log=f"./{MODEL_PREFIX}_{ENV_NAME}_tensorboard/",
+        )
     )
-    model.learn(total_timesteps=50000)
+
+    checkpoint_callback = CheckpointCallback(
+        save_freq=1000,
+        save_path=f"./{MODEL_PREFIX}_{ENV_NAME}_checkpoints/",
+        name_prefix=f"{MODEL_PREFIX}_{ENV_NAME}",
+    )
+    model.learn(total_timesteps=50000, callback=checkpoint_callback)
 
     # Don't forget to save the VecNormalize statistics when saving the agent
-    model.save(log_dir + "ppo_" + ENV_NAME)
+    model.save(f"./{MODEL_PREFIX}_{ENV_NAME}_model")
     env.save(stats_path)
 
 # Load the agent
-model = PPO.load(log_dir + "ppo_" + ENV_NAME)
+model = _model.load(f"./{MODEL_PREFIX}_{ENV_NAME}_model")
 
 # Load the saved statistics
 env = make_vec_env(ENV_NAME, n_envs=1, env_kwargs={"render": True})
@@ -37,15 +51,14 @@ env = VecNormalize.load(stats_path, env)
 env.training = False
 # reward normalization is not needed at test time
 env.norm_reward = False
+env.render()
 
 obs = env.reset()
 for i in range(1000):
     action, _state = model.predict(obs, deterministic=True)
     obs, reward, done, info = env.step(action)
-    env.render()
-    time.sleep(1 / 24)
+    time.sleep(1 / 60)
     if done:
-        print("Done now!")
         obs = env.reset()
 
 from stable_baselines3.common.evaluation import evaluate_policy
