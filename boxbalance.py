8d1f902bd3d1332c3461bfc67841d49be43011e7
mhallac <matthew.hallac@berkeley.edu>
2021-04-24 22:49:01 -0400

    basic box balance

--- boxbalance.py
+++ boxbalance.py
@@ -5,16 +5,43 @@ import numpy as np
 
 class BoxBot:
     def __init__(self, startPos, startOrientation):
-        self.boxId = p.loadURDF("boxbot.urdf", startPos, startOrientation)
+        self.id = p.loadURDF("boxbot.urdf", startPos, startOrientation)
+        self.desiredPosEuler = [np.pi/4, 0, 0]
         
     def simulate(self, time_steps):
-        print(self.getPose())
         for i in range(time_steps):
+            print(i)
+            if i > 500:
+                self.controlTorque()
             p.stepSimulation()
             time.sleep(1.0 / 240.0)
     
     def getPose(self):
-        return p.getBasePositionAndOrientation(self.boxId)
+        # Returns a tuple of (position, quaternion)
+        return p.getBasePositionAndOrientation(self.id)
+
+    def getEulerOrientation(self):
+        return p.getEulerFromQuaternion(self.getPose()[1])
+    
+    def setTorque(self, torque):
+        p.setJointMotorControl2(bodyIndex=self.id,
+                                        jointIndex=0,
+                                        controlMode=p.TORQUE_CONTROL,
+                                        force=torque)
+
+    def setVelocity(self, velocity, maxForce=500):
+        p.setJointMotorControl2(bodyIndex=self.id,
+                                jointIndex=0,
+                                controlMode=p.TORQUE_CONTROL,
+                                targetVelocity=velocity,
+                                force=maxForce)
+    
+    def controlTorque(self):
+        orientation = self.getEulerOrientation()
+        if orientation[0] < self.desiredPosEuler[0]:
+            self.setTorque(-1)
+        elif orientation[0] > self.desiredPosEuler[0]:
+            self.setTorque(1)
 
 def main():
     print("STARTING SIM")
