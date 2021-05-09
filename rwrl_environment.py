09203b9d5e65877c38b5d05e6889f3f8adf350a1
Jaiveer Singh <j.singh@berkeley.edu>
2021-05-08 19:27:37 -0700

    Clean up tree

--- rwrl_environment.py
+++
@@ -1,9 +0,0 @@
-import pybullet as p
-import pybullet_data
-
-client = p.connect(p.GUI)
-
-p.setGravity(0, 0, -10, physicsClientId=client)
-
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-planeId = p.loadURDF("plane.urdf")
